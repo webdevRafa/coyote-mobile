852cd709b1bd41322c168cf33a5082647c578c94
Rafael Castro <ralphc1992@live.com>
2025-01-15 23:35:39 -0600

    updating front-end

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,19 @@
 export const Navbar: React.FC = () => {
   return (
     <>
-      <div className="w-full bg-dark-gray fixed top-0 left-0 px-4 z-50">
+      <div className="w-full bg-dark-gray fixed top-0 left-0 px-4 z-50 pb-3 fades">
         <nav className="w-full h-full flex justify-center">
-          <ul className="flex font-bold text-sky">
-            <li className="cursor-pointer hover:text-white hover:bg-gray hover:scale-110 w-full h-full p-5 transition-all duration-150 ease-in-out">
+          <ul className="flex font-bold text-sky fadeIn">
+            <li className="cursor-pointer hover:text-white hover:bg-gradient-to-b from-gray to-dark-gray hover:scale-105 w-full h-full p-5 transition-all duration-150 ease-in-out">
               Home
             </li>
-            <li className="cursor-pointer hover:text-white hover:bg-gray hover:scale-110 w-full h-full p-5 transition-all duration-150 ease-in-out">
+            <li className="cursor-pointer hover:text-white hover:bg-gradient-to-b from-gray to-dark-gray hover:bg-gray hover:scale-105 w-full h-full p-5 transition-all duration-150 ease-in-out">
               About
             </li>
-            <li className="cursor-pointer hover:text-white hover:bg-gray hover:scale-110 w-full h-full p-5 transition-all duration-150 ease-in-out">
+            <li className="cursor-pointer hover:text-white hover:bg-gradient-to-b from-gray to-dark-gray hover:bg-gray hover:scale-105 w-full h-full p-5 transition-all duration-150 ease-in-out">
               Dashboard
             </li>
-            <li className="cursor-pointer hover:text-white hover:bg-gray hover:scale-110 w-full h-full p-5 transition-all duration-150 ease-in-out">
+            <li className="cursor-pointer hover:text-white hover:bg-gradient-to-b from-gray to-dark-gray hover:bg-gray hover:scale-105 w-full h-full p-5 transition-all duration-150 ease-in-out">
               Contact
             </li>
           </ul>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,26 +4,31 @@ export const About: React.FC = () => {
   return (
     <>
       <div className="my-[-2px] bg-dark-gray py-10 flex flex-col items-center md:flex-row w-full px-[10px] gap-2  mb-40 z-40 relative max-w-[1200px] mx-auto">
-        <div className="bg-gray">
-          <img
-            className="mx-auto shadow-md md:translate-x-[-50%] slideRight "
-            src={doc}
-            alt=""
-          />
+        <div>
+          <img className="mx-auto shadow-md  slideRight " src={doc} alt="" />
         </div>
 
         <div className="w-full flex items-center justify-center px-6 mt-5 md:mt-0">
-          <p className="text-md text-left bg-gray shadow-md p-4 text-white">
-            At Coyote Mobile Chiropractic, we understand how hectic life can be.
-            That's why we bring high-quality chiropractic care directly to your
-            doorstep. Dr. Moises Hernandez, D.C., is committed to helping you
-            feel your best without the hassle of traveling to a clinic. With a
-            focus on personalized care, Dr. Hernandez uses his expertise to
-            provide effective adjustments and muscle therapy tailored to your
-            unique needs. Whether you're dealing with pain, stiffness, or just
-            looking to improve your overall mobility, our services are designed
-            to help you regain control of your health and feel at ease.
-          </p>
+          <div>
+            <p className="text-xl text-left bg-gray  shadow-md p-4 text-white">
+              At Coyote Mobile Chiropractic, we understand how hectic life can
+              be.
+            </p>
+            <p className="text-blue font-bold mt-5 text-lg">
+              That's why we bring high-quality chiropractic care directly to
+              your doorstep.
+            </p>
+            <p className="text-off-white mt-5 text-lg">
+              Dr. Moises Hernandez, D.C., is committed to helping you feel your
+              best without the hassle of traveling to a clinic. With a focus on
+              personalized care, Dr. Hernandez uses his expertise to provide
+              effective adjustments and muscle therapy tailored to your unique
+              needs. Whether you're dealing with pain, stiffness, or just
+              looking to improve your overall mobility, our services are
+              designed to help you regain control of your health and feel at
+              ease.
+            </p>
+          </div>
         </div>
       </div>
     </>
